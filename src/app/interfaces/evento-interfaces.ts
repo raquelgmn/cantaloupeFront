12dea847bdee4da561f8679393dffe070ed497b2
raquelgmn <raquelgarciamonsalve@gmail.com>
2023-01-25 20:06:07 +0100

    Revert "Se eliminan las modificaciones eran pruebas."
    This reverts commit 1390df2ccc0f166b3525b7a4a6a72f359dbaae63.

--- src/app/interfaces/evento-interfaces.ts
+++ src/app/interfaces/evento-interfaces.ts
@@ -6,3 +6,20 @@ export interface Evento {
     description: string;
     timestamp: any;
 }
+
+
+export interface Evento {
+    id: any;
+    event_date: any;
+    event_time: any;
+    description: string;
+    timestamp: any;
+}
+
+export interface Evento {
+    id: any;
+    event_date: any;
+    event_time: any;
+    description: string;
+    timestamp: any;
+}
